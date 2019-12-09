bfc005a4c1a73fef35f08c32f11e338cfb2a381e
Aaron Shackelford <burntcreamlatte@icloud.com>
2019-12-09 14:48:46 -0700

    implement Entry List table view controller; complete storyboard

---
+++ CKJournal/Controllers/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EntryDetailViewController.swift
+//  CKJournal
+//
+//  Created by Aaron Shackelford on 12/9/19.
+//  Copyright © 2019 Aaron Shackelford. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ CKJournal/Controllers/View Controllers/EntryListTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  EntryListTableViewController.swift
+//  CKJournal
+//
+//  Created by Aaron Shackelford on 12/9/19.
+//  Copyright © 2019 Aaron Shackelford. All rights reserved.
+//
+
+import UIKit
+
+class EntryListTableViewController: UITableViewController {
+
+    // MARK: - Lifecycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateViews()
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return EntryController.shared.entries.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
+        
+        let entry = EntryController.shared.entries[indexPath.row]
+        cell.textLabel?.text = entry.title
+        cell.detailTextLabel?.text = entry.timestamp.formattedString()
+        
+        return cell
+    }
+    
+    // MARK: - Helper Methods
+    func updateViews() {
+        EntryController.shared.fetchEntries { (success) in
+            if success {
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //iidoo
+    }
+}
